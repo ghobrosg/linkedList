b5dd13ac87e30e9a0c8f3307b69f747fa82a0590
ghobrosg <94204190+ghobrosg@users.noreply.github.com>
2023-12-28 22:50:09 -0500

    Add files via upload

---
+++ LinkedList.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.left = Node(0)
+        self.right = Node(0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def get(self, index: int) -> int:
+        curr = self.left.next
+        while curr and index > 0:
+            curr = curr.next
+            index -= 1
+        if curr and curr != self.right and index == 0:
+            return curr.val
+        return -1
+
+    def addAtHead(self, val: int) -> None:
+        node, next, prev = Node(val), self.left.next, self.left
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = prev
+
+    def addAtTail(self, val: int) -> None:
+        node, next, prev = Node(val), self.right, self.right.prev
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = prev
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        curr = self.left.next
+        while curr and index > 0:
+            curr = curr.next
+            index -= 1
+        if curr and index == 0:
+            node, next, prev = Node(val), curr, curr.prev
+            prev.next = node
+            next.prev = node
+            node.next = next
+            node.prev = prev
+
+    def deleteAtIndex(self, index: int) -> None:
+        curr = self.left.next
+        while curr and index > 0:
+            curr = curr.next
+            index -= 1
+        if curr and curr != self.right and index == 0:
+            next, prev = curr.next, curr.prev
+            next.prev = prev
+            prev.next = next
